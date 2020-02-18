1f7c6f1602d9af104ae9a2c48deedd168d086fa8
rakibgalib <rakibhossaingalib@gmail.com>
2020-02-18 20:27:32 +0600

    Ajax Call Initially done

--- FrontEnd/js/transactionTable.js
+++ FrontEnd/js/transactionTable.js
@@ -1,13 +1,22 @@
+var id = $("#customerNumberTextbox").val();
+var start = $("#startDate").val();
+var end = $("#endDate").val();
+var totalDebit = 0;
+var totalCredit = 0;
+var drCount = 0;
+var crCount = 0;
 $("#showAccountbtn").click(function() {
-    let id = $("#customerNumberTextbox").val();
-    let start = $("#startDate").val();
-    let end = $("#endDate").val();
+
+
+    // console.log(id, start, end);
 
     let customerInfo = {
         customerId: id,
         startDate: start,
         endDate: end
     };
+    // console.log(customerInfo);
+
 
     $.ajax({
         type: "POST",
@@ -15,12 +24,39 @@ $("#showAccountbtn").click(function() {
         data: customerInfo,
         dataType: "json",
         success: function(response) {
-            console.log(response);
+
+            for (var i = 0; i < response.transactions.length; i++) {
+                totalDebit = totalDebit + response.transactions[i].debit;
+                if (response.transactions[i].debit > 0) {
+                    drCount += 1;
+                }
+                response.transactions[i].debit = (response.transactions[i].debit.toString()) + ".00";
+
+                totalCredit = totalCredit + response.transactions[i].credit;
+                if (response.transactions[i].credit > 0) {
+                    crCount += 1;
+                }
+                response.transactions[i].credit = (response.transactions[i].credit.toString()) + ".00";
+
+                if (response.transactions[i].reference == "NULL") {
+                    response.transactions[i].reference = '';
+                }
+            }
+
+            $("#finalDebits").html(totalDebit.toString() + ".00");
+            $("#finalCredits").html(totalCredit.toString() + ".00");
+            $("#openingBalanceFinal").html(response.accountOpeningBalance);
+            $("#drCount").html(drCount);
+            $("#crCount").html(crCount);
 
             $("#mainTable").DataTable({
+                "paging": false,
+                searching: false,
+                "info": false,
+                "ordering": false,
                 data: response.transactions,
                 columns: [{
-                    'data': 'transactionDate'
+                    'data': 'date'
                 }, {
                     'data': 'brn'
                 }, {
@@ -28,14 +64,36 @@ $("#showAccountbtn").click(function() {
                 }, {
                     'data': 'reference'
                 }, {
-                    'data': 'debits'
+                    'data': 'debit'
                 }, {
-                    'data': 'credits'
+                    'data': 'credit'
                 }, {
                     'data': 'balance'
                 }]
             });
+        },
+        error: function(response) {
+            console.log(response);
 
+        }
+    });
+
+    $.ajax({
+        type: "GET",
+        url: "http://127.0.0.1:9091/account/" + id,
+        dataType: "json",
+        success: function(response) {
+            console.log(response);
+            $("#accountNumber").html(response.accountInfo.accountNumber);
+            $("#startDat").html(start);
+            $("#endDat").html(end);
+            $("#customerId").html(response.accountInfo.customer.id);
+            $("#accountHolderName").html(response.accountInfo.customer.customerName);
+            $("#accountHolderAddressHouse").html(response.accountInfo.customer.address);
+
+        },
+        error: function(response) {
+            console.log(response);
         }
     });
 });
