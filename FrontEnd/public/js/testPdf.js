346c8a00cbd018ba9fd8290e5b6d9069c8644d7b
naushad.hossain <naushad.hossain@leads-bd.com>
2020-02-23 10:08:13 +0600

    pdf formatted newly

--- FrontEnd/public/js/testPdf.js
+++ FrontEnd/public/js/testPdf.js
@@ -1,18 +1,18 @@
-$(document).ready(function () {
+$(document).ready(function() {
     const btnCreatePdf = $("#createPdfbtn");
     const COL_WIDTH = 48.867;
     const margins = {
         top: 25.4,
         bottom: 25.4,
         left: 31.7,
-        width: 146.6
+        width: 146.6,
     };
     let currentMarginTop = margins.top;
     const lineHeigth = 3;
 
     let pdf = new jsPDF("p", "mm", [210, 297]);
 
-    btnCreatePdf.click(function (e) {
+    btnCreatePdf.click(function(e) {
         e.preventDefault();
 
         //adding first row
@@ -38,12 +38,12 @@ $(document).ready(function () {
             $("#bankBranchName").text(),
             $("#bankBranchAddressRoad").text(),
             $("#BankBranchAddressArea").text(),
-            $("#BankBranchAddressCity").text()
+            $("#BankBranchAddressCity").text(),
         ];
 
         const numOfColumn = 3;
         const colWidth = margins.width / numOfColumn;
-        const marginLeft = margins.left + colWidth;
+        const marginLeft = margins.left + colWidth / 2;
         const marginTop = currentMarginTop;
         let rowHeight = 0;
 
@@ -65,9 +65,9 @@ $(document).ready(function () {
         const firstColInfo = [
             $("#accountHolderName").text(),
             $("#accountHolderAddressHouse").text(),
-            formatText($("#accountHolderAddressRoad").text()),
-            formatText($("#accountHolderAddressArea").text()),
-            formatText($("#accountHolderPostCode").text())
+            $("#accountHolderAddressRoad").text(),
+            $("#accountHolderAddressArea").text(),
+            $("#accountHolderPostCode").text(),
         ];
         const thirdColInfo = [
             `Acount Number`,
@@ -75,43 +75,43 @@ $(document).ready(function () {
             `Page`,
             `Currency Name`,
             `Branch Code`,
-            `Customer Id`
+            `Customer Id`,
         ];
-        const fourthColInfo = [':', ':', ':', ':', ':', ':',];
+        const fourthColInfo = [":", ":", ":", ":", ":", ":"];
         const fifththColInfo = [
             $("#accountNumber").text(),
             formatText($("#timePeriod").text()),
             $("#pageCount").text(),
             formatText($("#currencyName").text()),
             $("#branchCode").text(),
-            $("#customerId").text()
+            $("#customerId").text(),
         ];
 
         colHeight = addColumn(
             firstColInfo,
             margins.left / 2,
             currentMarginTop,
-            colWidth
+            colWidth,
         );
         rowHeight = adjustRowHeight(rowHeight, colHeight);
 
         colHeight = addColumn(
             thirdColInfo,
-            margins.left + (colWidth * 2),
+            margins.left + colWidth * 2,
             currentMarginTop,
-            colWidth
+            colWidth,
         );
         colHeight = addColumn(
             fourthColInfo,
             margins.left + (colWidth * 8) / 3.2,
             currentMarginTop,
-            colWidth
+            colWidth,
         );
         colHeight = addColumn(
             fifththColInfo,
             margins.left + (colWidth * 8) / 3.1,
             currentMarginTop,
-            colWidth
+            colWidth,
         );
         rowHeight = adjustRowHeight(rowHeight, colHeight);
         currentMarginTop = currentMarginTop + rowHeight + lineHeigth;
@@ -121,23 +121,27 @@ $(document).ready(function () {
         const numOfColumn = 4;
         const colWidth = margins.width / numOfColumn;
         let rowHeight = lineHeigth;
-        addText("ONLINE STATEMENT", margins.left + colWidth / 2, currentMarginTop, colWidth);
+        addText(
+            "ONLINE STATEMENT",
+            margins.left + colWidth,
+            currentMarginTop,
+            colWidth,
+        );
         currentMarginTop = currentMarginTop + rowHeight + lineHeigth;
     }
 
     function addFourthRow() {
-
-        let table = $('#mainTable');
+        let table = $("#mainTable");
         let data = pdf.autoTableHtmlToJson(table[0]);
         console.log(data);
 
         pdf.autoTable(data.columns, data.rows, {
             tableLineColor: [255, 255, 255],
-            tableLineWidth: 0.20,
+            tableLineWidth: 0.2,
             styles: {
-                font: 'times',
+                font: "times",
                 lineColor: [255, 255, 255],
-                lineWidth: .20
+                lineWidth: 0.2,
             },
             headStyles: {
                 fillColor: [255, 255, 255],
@@ -154,28 +158,36 @@ $(document).ready(function () {
                 textColor: [0, 0, 0],
                 fontSize: 8,
             },
-            alternateRowStyles: {
-
-            },
+            alternateRowStyles: {},
             columnStyles: {
-                4: { halign: 'right' },
-                5: { halign: 'right' },
-                6: { halign: 'right' },
+                4: { halign: "right" },
+                5: { halign: "right" },
+                6: { halign: "right" },
             },
             startY: currentMarginTop,
         });
 
         currentMarginTop = pdf.previousAutoTable.finalY + lineHeigth;
-        const endingStatement = $('#endingStatement').text();
+        const endingStatement = $("#endingStatement").text();
         const numberOfCols = 10;
         const colWidth = margins.width / numberOfCols;
-        addText(endingStatement, (margins.left + (colWidth * 10)), currentMarginTop, colWidth);
+        addText(
+            endingStatement,
+            margins.left + colWidth * 10,
+            currentMarginTop,
+            colWidth,
+        );
         currentMarginTop = currentMarginTop + lineHeigth * 3;
     }
     function addFifthRow() {
         const numOfColumn = 4;
-        addText("STATEMENT CLOSING BALANCE", margins.left + getColWidth(numOfColumn) / 2, currentMarginTop, getColWidth(numOfColumn) * 2);
-        currentMarginTop += (2 * lineHeigth);
+        addText(
+            "STATEMENT CLOSING BALANCE",
+            margins.left,
+            currentMarginTop,
+            getColWidth(numOfColumn) * 2,
+        );
+        currentMarginTop += 2 * lineHeigth;
     }
 
     function addSixthRow() {
@@ -183,35 +195,68 @@ $(document).ready(function () {
         const colWidth = getColWidth(numOfCols);
         let rowHeight = 0;
         let colHeight = 0;
-        const firstColInfo = ["OPENNING BANALNCE", "DEBITS", "CREDITS", "UNCOLLECTED FUNDS"];
-        const secondColInfo = [
-            $('#openingBalanceFinal').text(),
-            $('#finalDebits').text(),
-            $('#finalCredits').text(),
-            formatText($('#uncollectedFunds').text()),
+        const firstColInfo = [
+            "OPENNING BANALNCE",
+            "DEBITS",
+            "CREDITS",
+            "UNCOLLECTED FUNDS",
         ];
-        const thirdColInfo = ['DRCOUNT', 'CRCOUNT'];
-        const fourthColInfo = [$('#drCount').text(), $('#crCount').text()];
+        const secondColInfo = [
+            $("#openingBalanceFinal").text(),
+            $("#finalDebits").text(),
+            $("#finalCredits").text(),
+            formatText($("#uncollectedFunds").text()),
+        ];
+        const thirdColInfo = ["DRCOUNT", "CRCOUNT"];
+        const fourthColInfo = [$("#drCount").text(), $("#crCount").text()];
 
-        colHeight = addColumn(firstColInfo, margins.left + colWidth / 2, currentMarginTop, colWidth);
-        rowHeight = adjustRowHeight(rowHeight, colHeight)
-        colHeight = addColumn(secondColInfo, margins.left + (colWidth * 3) / 2, currentMarginTop, colWidth);
+        colHeight = addColumn(
+            firstColInfo,
+            margins.left,
+            currentMarginTop,
+            colWidth,
+        );
         rowHeight = adjustRowHeight(rowHeight, colHeight);
-        colHeight = addColumn(thirdColInfo, margins.left + colWidth * 2, currentMarginTop + lineHeigth, colWidth);
-        colHeight = addColumn(fourthColInfo, margins.left + (colWidth * 5) / 2, currentMarginTop + lineHeigth, colWidth);
+        colHeight = addColumn(
+            secondColInfo,
+            margins.left + colWidth,
+            currentMarginTop,
+            colWidth,
+        );
+        rowHeight = adjustRowHeight(rowHeight, colHeight);
+        colHeight = addColumn(
+            thirdColInfo,
+            margins.left + (colWidth * 3) / 2,
+            currentMarginTop + lineHeigth,
+            colWidth,
+        );
+        colHeight = addColumn(
+            fourthColInfo,
+            margins.left + colWidth * 2,
+            currentMarginTop + lineHeigth,
+            colWidth,
+        );
         currentMarginTop = currentMarginTop + rowHeight + lineHeigth;
-        addText('*   =   UNAUTH ENTRY   /   R   =   REVERSAL', margins.left + colWidth / 2, currentMarginTop);
+        addText(
+            "*   =   UNAUTH ENTRY   /   R   =   REVERSAL",
+            margins.left,
+            currentMarginTop,
+        );
         currentMarginTop = currentMarginTop + lineHeigth * 2;
-        addText(`- - - - - - - - - - - - - - - - - - - - - - - - END OF STATEMENT - - - - - - - - - - - - - - - - - - - - - - - -`,
-            margins.left + colWidth / 2, currentMarginTop, colWidth * 3);
+        addText(
+            `- - - - - - - - - - - - - - - - - - - - - - - - END OF STATEMENT - - - - - - - - - - - - - - - - - - - - - - - -`,
+            margins.left + colWidth / 2,
+            currentMarginTop,
+            colWidth * 3,
+        );
         currentMarginTop = currentMarginTop + lineHeigth * 2;
     }
 
     function addSeventhRow() {
-        const text = `Please note that any dispency must be notified to the bank within 15 days from the date of this statement.Else it will be deemed that the customer has found this statementis corret.`
+        const text = `Please note that any dispency must be notified to the bank within 15 days from the date of this statement.Else it will be deemed that the customer has found this statementis corret.`;
         const numOfCols = 1;
         const colWidth = getColWidth(numOfCols);
-        addText(text, margins.left, currentMarginTop, colWidth);
+        addText(text, margins.left / 2, currentMarginTop, colWidth);
     }
 
     function getColWidth(numOfCols) {
@@ -239,23 +284,22 @@ $(document).ready(function () {
 
     function formatText(text) {
         console.log(text);
-        text.replace('\n', '');
-        let textArray = text.split(' ');
-        let finaltext = '';
+        text.replace("\n", "");
+        let textArray = text.split(" ");
+        let finaltext = "";
         textArray.forEach(element => {
-            if (element !== '') {
-                finaltext = finaltext + element.replace('\n', '') + ' ';
+            if (element !== "") {
+                finaltext = finaltext + element.replace("\n", "") + " ";
             }
         });
         console.log(finaltext);
         return finaltext.substring(1);
-
     }
 
     function addText(text, marginLeft, marginTop, colwidth) {
         const lines = pdf.splitTextToSize(text, colwidth, {
             orientation: "p",
-            lineHeight: lineHeigth
+            lineHeight: lineHeigth,
         });
 
         pdf.text(marginLeft, marginTop, lines);
